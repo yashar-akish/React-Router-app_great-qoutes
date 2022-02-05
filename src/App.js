20f4b6c2c1d2db3f0841dcace559abf586f00fb4
Yashar Akish <yashar_akish@yahoo.com>
2022-02-05 18:00:29 -0500

    added "Redirect" and used useParams hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch  } from 'react-router-dom';
+import { Route, Switch, Redirect  } from 'react-router-dom';
 
 
 import AllQuotes from './pages/AllQuotes';
@@ -8,6 +8,7 @@ import NewQuote from './pages/NewQuote';
 function App() {
   return (
     <Switch>
+      <Route path='/' exact><Redirect to='/quotes' /></Route>
       <Route path='/quotes' exact>
         <AllQuotes />
       </Route>
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,9 +1,14 @@
-
+import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const QuoteDetail = () => {
+  const params = useParams();
   return (
-    <h1>Quote Detail Page</h1>
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
   )
 };
 
