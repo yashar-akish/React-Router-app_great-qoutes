ed09596edb1e656eca8f82f69d00230e55298883
Yashar Akish <yashar_akish@yahoo.com>
2022-02-06 10:48:45 -0500

    added function to send qoute from newQuote to firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,15 +1,27 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+
+  const {sendRequest, status} = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
+
   const addQuoteHandler = quoteData => {
-    console.log(quoteData);
-    history.push('/quotes');
+    sendRequest(quoteData);
   }
   return (
-    <QuoteForm onAddQuote={addQuoteHandler}/>
+    <QuoteForm isLoading={status==='pending'} onAddQuote={addQuoteHandler}/>
   )
 };
 
