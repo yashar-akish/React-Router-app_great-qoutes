58f23086aca078d317d387b2ff69d5b93ece95ee
Yashar Akish <yashar_akish@yahoo.com>
2022-02-05 21:55:50 -0500

    added a function to dispalys the quotes asc and desc order

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -4,6 +4,19 @@ import { useHistory, useLocation } from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
+
 const QuoteList = (props) => {
 
   const history = useHistory();
@@ -11,6 +24,8 @@ const QuoteList = (props) => {
 
   const queryParams = new URLSearchParams(location.search);
   const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
     history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
@@ -22,7 +37,7 @@ const QuoteList = (props) => {
         <button onClick={changeSortingHandler}>Sort {isSortingAscending ? 'Descending' : "Ascending"}</button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
