61ca0526e4f938bd3e1fb7d9d9645c367e42c535
Yashar Akish <yashar_akish@yahoo.com>
2022-02-05 19:40:42 -0500

    displaed quoteForm in NewQuote component

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,13 @@
 
-
+import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+  }
   return (
-    <h1>New Quote Page</h1>
+    <QuoteForm onAddQuote={addQuoteHandler}/>
   )
 };
 
