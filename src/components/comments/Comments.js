d631ffb07b77383c6f6ba0d016c660b13ca5ac7d
Yashar Akish <yashar_akish@yahoo.com>
2022-02-06 12:54:27 -0500

    added comment to display in comment page and generate new comment

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams }from 'react-router-dom';
 
 import classes from './Comments.module.css';
+
 import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
+import LoadingSpinner from '../UI/LoadingSpinner';
+
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
 
+  const params = useParams();
+
+  const { sendRequest, status, data: loadedComments} = useHttp(getAllComments);
+
+  const { quoteId } = params;
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const AddedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest,quoteId])
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments}/>
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className="centered">No comments were added yet</p>
+  }
   
   return (
     <section className={classes.comments}>
@@ -18,8 +56,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={AddedCommentHandler}/>}
+      {comments} 
     </section>
   );
 };
