ee48d3a82ee65af6fdf3b774f5aa293b19ca854c
Yashar Akish <yashar_akish@yahoo.com>
2022-02-05 19:56:57 -0500

    displayed QoteDetail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,19 +2,35 @@ import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'yashar',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: 'q2',
+    author: 'akish',
+    text: 'Etiam imperdiet leo sed dolor pulvinar consequat. Etiam tempor imperdiet magna.',
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((qut) => qut.id === params.quoteId);
+  if (!quote) {
+    return <p>No quote found</p>
+  }
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </Fragment>
-  )
+  );
 };
 
 export default QuoteDetail;
