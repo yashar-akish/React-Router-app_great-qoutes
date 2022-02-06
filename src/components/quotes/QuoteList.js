9c74eedaf81e93ae1ce8f8d77e0f82f97196da41
Yashar Akish <yashar_akish@yahoo.com>
2022-02-05 22:25:38 -0500

    made all the URLs and paths more dynamic

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -28,7 +28,11 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+    history.push({ 
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`
+    });
+    // history.push(`${location.pathname}?sort=${(isSortingAscending ? 'desc' : 'asc')}`);    //alternative way
   };
 
   return (
