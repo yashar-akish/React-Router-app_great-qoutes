7cf5e7703605b2e13fe83ee8b1c190c6567a9158
Yashar Akish <yashar_akish@yahoo.com>
2022-02-06 11:04:05 -0500

    loadded quotes from firebase removed dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,39 @@
+import { useEffect } from 'react';
 
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
-const DUMMY_QUOTES = [
-  {id: 'q1', author: 'yashar', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
-  {id: 'q2', author: 'akish', text:'Etiam imperdiet leo sed dolor pulvinar consequat. Etiam tempor imperdiet magna.'}
-]
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+// const DUMMY_QUOTES = [
+//   {id: 'q1', author: 'yashar', text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+//   {id: 'q2', author: 'akish', text:'Etiam imperdiet leo sed dolor pulvinar consequat. Etiam tempor imperdiet magna.'}
+// ]
 
 
 const AllQuotes = () => {
-  return (
-    <QuoteList quotes={DUMMY_QUOTES}/>
-  )
+
+  const {sendRequest, status, data: loadedQuotes, error} = useHttp(getAllQuotes, true);
+
+  useEffect(() => {sendRequest();}, [sendRequest]);
+
+  if (status === 'pending') {
+    return(
+      <div className="centered"><LoadingSpinner /></div>
+    )
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>
+  }
+
+  if (status==='completed' && (!loadedQuotes || loadedQuotes.length===0)) {
+    return <NoQuotesFound />
+  }
+
+  return <QuoteList quotes={loadedQuotes} />
 };
 
 export default AllQuotes;
